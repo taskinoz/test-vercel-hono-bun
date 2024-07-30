7c7f1c953ccb8843125829322553442342fce898
Tristan Kennedy <tristan@taskinoz.com>
2024-07-30 19:50:51 +1000

    Updated paths

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ export const config = {
   runtime: 'edge'
 }
 
-const app = new Hono().basePath('/')
+const app = new Hono().basePath('/api')
 
 app.get('/', (c) => {
   return c.json({ message: 'Hello Hono!' })
